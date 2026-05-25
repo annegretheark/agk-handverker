4c334026f8b604df4185f02932e99327a4a17ff2
Anne Grethe <anne-grethe.knutsen@atea-no>
2026-05-25 13:10:30 +0200

    Legg til modulgrunnlag

--- js/jsbk/backup.js
+++
@@ -1,5 +0,0 @@
-function backup() { alert("Backup er ikke endret i denne fikspakken."); }
-function importerBackup() { alert("Import er ikke endret i denne fikspakken."); }
-window.backup = backup;
-window.importerBackup = importerBackup;
-window.backup = backup;
